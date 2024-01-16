8c71839b95e4dfceee0cbf2ddf38addfab61d159
MBurakT <mbtbrk11@outlook.com>
2024-01-16 14:28:54 +0300

    BookStore | Apply EFCore.InMemory

--- BookStore/WebApi/Program.cs
+++ BookStore/WebApi/Program.cs
@@ -1,7 +1,10 @@
-// dotnet watch run --project WebApi
+// dotnet watch run --project WebApi/WebApi.csproj
+using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApi.DBOperations;
 
 namespace WebApi;
 
@@ -12,13 +15,20 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
         builder.Services.AddControllers();
+        builder.Services.AddDbContext<BookStoreDbContext>(opt => opt.UseInMemoryDatabase("BookStoreDB"));
+        //builder.Services.AddTransient<DataSeeder>();
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
+
+        using (IServiceScope serviceScope = app.Services.CreateScope())
+        {
+            DataSeeder.Seed(serviceScope.ServiceProvider);
+        }
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
